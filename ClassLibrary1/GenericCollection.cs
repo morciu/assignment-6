269d03196b13223919b2fd80a9bb3f536ef866bb
morciu <marianpralea@gmail.com>
2022-07-30 19:15:48 +0300

    Assignment 6

---
+++ ClassLibrary1/GenericCollection.cs
@@ -0,0 +1,95 @@
+﻿namespace ClassLibrary1
+{
+    public class GenericCollection<T>
+    {
+        private readonly int _maxSize;
+        private int _currentIndex = -1;
+        private readonly T[] _items;
+
+        public GenericCollection(int maxSize = 5)
+        {
+            _maxSize = maxSize;
+            _items = new T[maxSize];
+        }
+        public int Length { get { return _items.Length; } }
+
+        public void GetFromIndex(int index)
+        {
+            if (index > _maxSize || index < 0)
+            {
+                throw new Exception($"Array does not have index: {index}");
+            }
+            Console.Write(_items[index]);
+        }
+
+        public void SetAtIndex(T item, int index)
+        {
+            if (index > _maxSize || index < 0)
+            {
+                throw new Exception($"Array does not have index: {index}");
+            }
+            _items[index] = item;
+        }
+
+        // Swapping by index
+        public void SwapTwoItems(int firstIndex, int secondIndex)
+        {
+            if (firstIndex >= _items.Length || secondIndex >= _items.Length || firstIndex < 0 || secondIndex < 0)
+            {
+                throw new Exception("Index Out of Range.");
+            }
+            T tempItem = _items[firstIndex];
+            _items[firstIndex] = _items[secondIndex];
+            _items[secondIndex] = tempItem;
+        }
+
+        // Swapping by index - item combination
+        public void SwapTwoItems(int firstIndex, T secondItem)
+        {
+            if (firstIndex < 0 || firstIndex >= _items.Length)
+            {
+                throw new Exception("Index out of range");
+            }
+            if (!_items.Contains(secondItem))
+            {
+                throw new Exception("Item does not exist in current collection");
+            }
+            T tempItem = _items[Array.IndexOf(_items, secondItem)];
+            _items[Array.IndexOf(_items, secondItem)] = _items[firstIndex];
+            _items[firstIndex] = tempItem;
+        }
+
+        // Swapping by items
+        public void SwapTwoItems(T firstItem, T secondItem)
+        {
+            if(!_items.Contains(firstItem) || !_items.Contains(secondItem))
+            {
+                throw new Exception("The items do not exist in the current collection.");
+            }
+            else
+            {
+                int firstIndex = Array.IndexOf(_items, firstItem);
+                int secondIndex = Array.IndexOf(_items, secondItem);
+
+                T tempItem = _items[firstIndex];
+                _items[firstIndex] = _items[secondIndex];
+                _items[secondIndex] = tempItem;
+            }
+        }
+
+        public void PrintCollection()
+        {
+            for(int i = 0; i < _items.Length; i++)
+            {
+                if (i == _maxSize - 1)
+                {
+                    Console.Write($"{_items[i]}.\n");
+                }
+                else
+                {
+                    Console.Write($"{_items[i]}, ");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClassLibrary1;
+
+// Instantiating Collection
+GenericCollection<string> genericCollection = new GenericCollection<string>();
+
+// Populating Collection
+genericCollection.SetAtIndex("hello", 0);
+genericCollection.SetAtIndex("hi", 1);
+genericCollection.SetAtIndex("Howdy", 2);
+genericCollection.SetAtIndex("Yo", 3);
+genericCollection.SetAtIndex("Sup", 4);
+
+genericCollection.PrintCollection();
+
+// Changing item values
+genericCollection.SetAtIndex("Good day", 2);
+genericCollection.SetAtIndex("How's it going", 4);
+
+genericCollection.PrintCollection();
+
+// Swapping Items By Index
+genericCollection.SwapTwoItems(2, 4);
+
+genericCollection.PrintCollection();
+
+// Swapping by item
+genericCollection.SwapTwoItems("hi", "Yo");
+
+genericCollection.PrintCollection();
+
+// Swapping by index - item combination
+genericCollection.SwapTwoItems(6, "Yo");
+
+genericCollection.PrintCollection();
+
+
+
